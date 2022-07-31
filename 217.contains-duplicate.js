af9425650b57dd2a141e546a006a18bd35ed0d6d
bappi2097 <bappi35-2097@diu.edu.bd>
2022-07-31 11:18:56 +0600

    217 contains duplicate problem solved

--- 217.contains-duplicate.js
+++ 217.contains-duplicate.js
@@ -9,8 +9,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    
+var containsDuplicate = function (nums) {
+  for (let i in nums) {
+    if (Number(i) + 1 <= nums.length && nums.includes(nums[i], Number(i) + 1)) {
+      return true;
+    }
+  }
+  return false;
 };
 // @lc code=end
 
+console.log(containsDuplicate([2, 14, 18, 22, 22]));
